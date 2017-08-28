0e12e9e3e9ed80159a0a1e4f73ee6c78dea943a3
ridewn <trevorstacy96@gmail.com>
2017-08-28 14:12:05 -0400

    Update build

--- src/DatePicker.vue
+++ src/DatePicker.vue
@@ -771,7 +771,7 @@
         // used instead of the current day.
         let date
 
-        if (this.min && this.min.trim() && !this.value) {
+        if (this.min && this.min.trim() && ! this.value) {
           date = new Date(this.min)
         } else if (this.value && this.value.trim()) {
           date = new Date(this.value)
